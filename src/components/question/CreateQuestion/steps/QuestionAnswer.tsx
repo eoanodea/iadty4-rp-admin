b1a5ef15a65790f6dc9e66f816fbfe74f922b54e
Eoan O Dea <odeaeoan@gmail.com>
2021-04-29 10:37:26 +0200

    Select Answer functionality for both text and multi choice

--- src/components/question/CreateQuestion/steps/QuestionAnswer.tsx
+++ src/components/question/CreateQuestion/steps/QuestionAnswer.tsx
@@ -1,20 +1,18 @@
 import {
-  Button,
   createStyles,
-  FormHelperText,
   makeStyles,
-  Fade,
   Theme,
   Typography,
   TextField,
-  FormControlLabel,
-  Switch,
-  FormControl,
+  Checkbox,
 } from "@material-ui/core";
+import {
+  CheckBoxOutlineBlank,
+  CheckBox as CheckBoxIcon,
+} from "@material-ui/icons";
 import { Autocomplete } from "@material-ui/lab";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuestion } from "..";
-import { QuestionValidator } from "../../../../types/question";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,47 +26,49 @@ const useStyles = makeStyles((theme: Theme) =>
 const QuestionAnswer = () => {
   const classes = useStyles();
   const [question, setQuestion] = useQuestion();
-  const [isMultiAnswer, setIsMultieAnswer] = useState(
-    question.type === "MULTIPLE_CHOICE"
-  );
 
-  const handleChange = (value: string) => {
+  const handleMultiChoiceChange = (value: string[]) => {
     let newQuestion = question;
-    newQuestion.answer = value.replace(" ", "_").toUpperCase();
+    newQuestion.answerArr = value;
 
     setQuestion(newQuestion);
   };
 
+  const handleTextChange = (value: string) => {
+    let newQuestion = question;
+    newQuestion.answer = value;
+
+    setQuestion(newQuestion);
+  };
+
+  const icon = <CheckBoxOutlineBlank fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
   return (
     <div className={classes.root}>
       <Typography variant="h1">Select Question Answer</Typography>
-      <FormControl component="fieldset">
-        <FormControlLabel
-          label={
-            isMultiAnswer ? "Multiple Options Answer" : "Single Option Answer"
-          }
-          control={
-            <Switch
-              checked={isMultiAnswer}
-              onChange={() => setIsMultieAnswer((o) => !o)}
-              name="Multiple Options Answers"
-            />
-          }
-        />
-        <FormHelperText>
-          {isMultiAnswer
-            ? "The user will enter a single answer"
-            : "The user will select multiple items as an answer"}
-        </FormHelperText>
-      </FormControl>
 
       {question.type === "MULTIPLE_CHOICE" ? (
         <Autocomplete
-          id="select-question-answer"
+          id="select-question-multi-choice-answer"
+          multiple
           options={question.options}
           getOptionLabel={(option) => option}
           style={{ width: 300 }}
-          onChange={(event, value) => handleChange(value || "")}
+          defaultValue={question.answerArr}
+          disableCloseOnSelect
+          onChange={(event, value) => handleMultiChoiceChange(value || "")}
+          renderOption={(option, { selected }) => (
+            <React.Fragment>
+              <Checkbox
+                icon={icon}
+                checkedIcon={checkedIcon}
+                style={{ marginRight: 8 }}
+                checked={selected}
+              />
+              {option}
+            </React.Fragment>
+          )}
           renderInput={(params) => (
             <TextField
               {...params}
@@ -78,7 +78,21 @@ const QuestionAnswer = () => {
           )}
         />
       ) : (
-        <h1>Not multi choice</h1>
+        <Autocomplete
+          id="select-question-text-answer"
+          defaultValue={question.answer}
+          options={question.options}
+          getOptionLabel={(option) => option}
+          style={{ width: 300 }}
+          onChange={(event, value) => handleTextChange(value || "")}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Select Question Answer"
+              variant="outlined"
+            />
+          )}
+        />
       )}
     </div>
   );
